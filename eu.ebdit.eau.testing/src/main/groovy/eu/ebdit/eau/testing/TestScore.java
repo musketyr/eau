7c83ca17a5e79b55696dca88bb77ca3c36c56440
vladimir.orany <vladimir.orany@7405c10c-9c5f-11de-b17c-2fd642447241>
2009-09-08 13:18:39 +0000

    fixed layout

---
+++ eu.ebdit.eau.testing/src/main/groovy/eu/ebdit/eau/testing/TestScore.java
@@ -0,0 +1,15 @@
+package eu.ebdit.eau.testing;
+
+public interface TestScore {
+
+	String getClassFQName();
+
+	String getTestName();
+
+	double getPoints();
+
+	String getMessage();
+
+	boolean isBonus();
+
+}
