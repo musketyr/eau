0bc3783f24dedaf11f44f5887deccfd9a3c75153
vladimir.orany <vladimir.orany@7405c10c-9c5f-11de-b17c-2fd642447241>
2009-09-08 15:05:14 +0000

    added test report parsing

---
+++ eu.ebdit.eau.testing/src/main/groovy/eu/ebdit/eau/testing/ResultBean.groovy
@@ -0,0 +1,16 @@
+package eu.ebdit.eau.testing
+
+
+
+/**
+ * @author Vladimír Oraný
+ *
+ */
+@Immutable public final class ResultBean implements Result{
+
+	String classFQName
+	String testName
+	Status status
+	String message
+	
+}
