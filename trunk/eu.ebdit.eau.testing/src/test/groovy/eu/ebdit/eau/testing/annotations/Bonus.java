60ea3f6053d3a4627c08255666f9c5da621f855d
vladimir.orany <vladimir.orany@7405c10c-9c5f-11de-b17c-2fd642447241>
2009-09-08 13:18:14 +0000

    fixed layout

--- trunk/eu.ebdit.eau.testing/src/test/groovy/eu/ebdit/eau/testing/annotations/Bonus.java
+++
@@ -1,5 +0,0 @@
-package eu.ebdit.eau.testing.annotations;
-
-public @interface Bonus {
-
-}
--- trunk/eu.ebdit.eau.testing/src/test/groovy/eu/ebdit/eau/testing/annotations/Details.java
+++
@@ -1,9 +0,0 @@
-package eu.ebdit.eau.testing.annotations;
-
-/**
- * Detailed description of given test.
- * @author Vladimir Orany
- */
-public @interface Details {
- /** @returns detailed description of given test */ String value();
-}
