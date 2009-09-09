a1503ee534f75b3ac3d535399724cb5de7bdc7a9
vladimir.orany <vladimir.orany@7405c10c-9c5f-11de-b17c-2fd642447241>
2009-09-09 09:44:49 +0000

    speeded up tests

--- trunk/eu.ebdit.eau.testing/src/test/java/eu/ebdit/eau/testing/TestFromXMLTest.java
+++ trunk/eu.ebdit.eau.testing/src/test/java/eu/ebdit/eau/testing/TestFromXMLTest.java
@@ -19,4 +19,9 @@ public class TestFromXMLTest extends BaseTest{
 	protected List<Result> getResultList() throws Exception{
 		return new TestReportXMLParser().parse(new File(getClass().getResource("/TestClass.xml").toURI()));
 	}
+	
+	@Override
+	public void testSelfCreateResult() throws Exception {
+		// do nothing
+	}
 }
