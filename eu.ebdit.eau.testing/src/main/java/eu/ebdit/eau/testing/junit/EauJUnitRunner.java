48b72039829b36ff14da34822a47bfba249ea1e3
vladimir.orany <vladimir.orany@7405c10c-9c5f-11de-b17c-2fd642447241>
2009-09-11 18:50:38 +0000

    added ability to record scores

--- eu.ebdit.eau.testing/src/main/java/eu/ebdit/eau/testing/junit/EauJUnitRunner.java
+++ eu.ebdit.eau.testing/src/main/java/eu/ebdit/eau/testing/junit/EauJUnitRunner.java
@@ -1,21 +1,15 @@
 package eu.ebdit.eau.testing.junit;
 
-import java.util.List;
-
 import org.junit.runner.JUnitCore;
-
-import com.google.common.collect.ImmutableList;
-
-import eu.ebdit.eau.testing.TestResult;
 
 public class EauJUnitRunner {
 
-    public List<TestResult> collectFrom(Class<?> classes) {
+    public EauRunListener collectResults(Class<?>... classes) {
 	EauRunListener erl = new EauRunListener();
 	JUnitCore core = new JUnitCore();
 	core.addListener(erl);
 	core.run(classes);
-	return ImmutableList.copyOf(erl.getResults());
+	return erl;
     }
 
 }
