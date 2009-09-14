da3834a0d99a0aa91ece9079307d5e21d9ade2a2
vladimir.orany <vladimir.orany@7405c10c-9c5f-11de-b17c-2fd642447241>
2009-09-14 20:38:16 +0000

    merged base and testing project

--- eu.ebdit.eau.testing/src/main/java/eu/ebdit/eau/testing/ResultCollector.java
+++
@@ -1,9 +0,0 @@
-package eu.ebdit.eau.testing;
-
-import eu.ebdit.eau.Result;
-
-public interface ResultCollector {
-
-    Iterable<Result> getResults();
-
-}
--- eu.ebdit.eau.testing/src/main/java/eu/ebdit/eau/testing/ScoreCollector.java
+++
@@ -1,9 +0,0 @@
-package eu.ebdit.eau.testing;
-
-import eu.ebdit.eau.Score;
-
-public interface ScoreCollector {
-
-    Iterable<Score> getScores();
-
-}
