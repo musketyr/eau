99e0f1ee04035cbcc8d053719ed183621f5e3502
vladimir.orany <vladimir.orany@7405c10c-9c5f-11de-b17c-2fd642447241>
2009-09-14 08:22:57 +0000

    trying to fix bug

--- trunk/eu.ebdit.eau.testing/src/test/java/eu/ebdit/eau/testing/annotations/ScoreAnnotationProcessorTest.java
+++ trunk/eu.ebdit.eau.testing/src/test/java/eu/ebdit/eau/testing/annotations/ScoreAnnotationProcessorTest.java
@@ -3,6 +3,7 @@ package eu.ebdit.eau.testing.annotations;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.LinkedList;
 import java.util.List;
@@ -20,7 +21,7 @@ import eu.ebdit.eau.testing.TestScore;
 public class ScoreAnnotationProcessorTest extends TestReporterTest {//NOPMD
 
     @Override
-    protected Iterable<TestScore> getScoreList() throws URISyntaxException  {//NOPMD
+    protected Iterable<TestScore> getScoreList() throws URISyntaxException, IOException  {//NOPMD
 	final ScoreAnnotationProcessor processor = new ScoreAnnotationProcessor();
 
 	// Get an instance of java compiler
@@ -29,14 +30,13 @@ public class ScoreAnnotationProcessorTest extends TestReporterTest {//NOPMD
 	// Get a new instance of the standard file manager implementation
 	final StandardJavaFileManager fileManager = compiler.getStandardFileManager(
 		null, null, null);
-
 	// Get the list of java file objects, in this case we have only
 	// one file, TestClass.java
-	final Iterable<? extends JavaFileObject> compilationUnits1 = fileManager.getJavaFileObjects(new File(ScoreAnnotationProcessorTest.class.getResource("/org/example/TestClass.java").toURI()));
+	final Iterable<? extends JavaFileObject> compilationUnits1 = fileManager.getJavaFileObjects(new File(getClass().getResource("/org/example/TestClass.java").toURI()));
 
 	final CompilationTask task = compiler.getTask(null, fileManager, null, null,
 		null, compilationUnits1);
-
+	
 	// Create a list to hold annotation processors
 	final List<Processor> processors = new LinkedList<Processor>();
 
