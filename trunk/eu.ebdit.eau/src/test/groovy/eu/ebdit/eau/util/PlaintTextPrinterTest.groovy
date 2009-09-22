d5d0bc015014a9d25ce872708cc54ef17c8d9f62
vladimir.orany <vladimir.orany@7405c10c-9c5f-11de-b17c-2fd642447241>
2009-09-22 17:51:18 +0000

    Update issue 3 Summary: Command line utility output specification Status: Started
    More details available at http://code.google.com/p/eau/issues/detail?id=3

--- trunk/eu.ebdit.eau/src/test/groovy/eu/ebdit/eau/util/PlaintTextPrinterTest.groovy
+++ trunk/eu.ebdit.eau/src/test/groovy/eu/ebdit/eau/util/PlaintTextPrinterTest.groovy
@@ -12,19 +12,19 @@ public class PlaintTextPrinterTest {
 
     private static final String TEXT_TO_MATCH =
 '''
-  50% Master Report
-        points:  2/4(6)        
-        details: This is root summary report
-     0% Child Report 1
-           points:  0/2(4)
-   100% Child Report 2
-           points:  2/2
-      100% Grand-child Report 2.1
-              points:  2/2
+ 50,00% Master Report
+        points: 2/4(6)
+        detail: This is root summary report
+     0,00% Child Report 1
+           points: 0/2(4)
+   100,00% Child Report 2
+           points: 2/2
+      100,00% Grand-child Report 2.1
+              points: 2/2
 '''
 
 
-   @Test @Ignore
+   @Test
     public void testPlainTextPrinter(){
         def writer = new StringWriter()
         new PlainTextPrinter().printReport(XmlReportPrinterTest.FIXURE, writer)
